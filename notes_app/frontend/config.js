ea2dae853f8e1ca5357530f209f0f6d41a527f39
RexTrue <114847416+RexTrue@users.noreply.github.com>
2026-06-04 19:31:13 +0700

    Update config.js

--- notes_app/frontend/config.js
+++ notes_app/frontend/config.js
@@ -1,3 +1,3 @@
 // Set this to the public URL of your backend service after deployment.
 // Example: window.API_BASE_URL = "https://your-cloud-run-service.a.run.app";
-window.API_BASE_URL = "https://rafa-dot-g-28-489306.as.r.appspot.com";
+window.API_BASE_URL = "https://rafa-100956722334.asia-southeast1.run.app";
