37ecb5d1df7ea37fdb2d41627740957a0a9720d9
RexTrue <114847416+RexTrue@users.noreply.github.com>
2026-06-09 20:41:29 +0700

    Update server.js

--- notes_app/backend/server.js
+++ notes_app/backend/server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/notes", notesRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
