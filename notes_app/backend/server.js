fc33aba0635d22c2aadcc1d765b210969255b8c7
RexTrue <114847416+RexTrue@users.noreply.github.com>
2026-06-09 22:17:20 +0700

    Update server.js

--- notes_app/backend/server.js
+++ notes_app/backend/server.js
@@ -1,32 +1,70 @@
 const express = require("express");
 const cors = require("cors");
+
 require("dotenv").config();
 
-const notesRoutes =
-  require("./routes/notes");
+const db =
+  require("./config/database");
+
+const noteRoutes =
+  require("./routes/noteRoutes");
+
+const NoteSchema =
+  require("./schema/Note");
 
 const app = express();
+
+const PORT =
+  process.env.PORT || 3000;
 
 app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend API Running");
+
+  res.send(
+    "Backend API Running"
+  );
 });
 
-app.use("/api/notes", notesRoutes);
-
-const PORT =
-  process.env.PORT || 3000;
+app.use("/api", noteRoutes);
 
 app.listen(
   PORT,
+
   "0.0.0.0",
-  () => {
+
+  async () => {
 
     console.log(
       `Server running on port ${PORT}`
     );
+
+    // Database connect setelah port terbuka
+    try {
+
+      await db.authenticate();
+
+      console.log(
+        "Database Connected..."
+      );
+
+      await NoteSchema.sync();
+
+      console.log(
+        "Database Synchronized..."
+      );
+
+    } catch (error) {
+
+      console.error(
+        "DATABASE ERROR:"
+      );
+
+      console.error(
+        error.message
+      );
+    }
   }
 );
