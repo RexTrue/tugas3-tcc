db9ff6747a7e9a64a0dea43bc3d843808db1b630
RexTrue <114847416+RexTrue@users.noreply.github.com>
2026-06-09 20:30:24 +0700

    Update server.js

--- notes_app/backend/server.js
+++ notes_app/backend/server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
+const notesRoutes = require("./routes/notes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const notesRoutes = require("./routes/notes");
+app.get("/", (req, res) => {
+  res.send("Backend API Running");
+});
 
 app.use("/api/notes", notesRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
