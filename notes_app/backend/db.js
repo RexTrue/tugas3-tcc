469cfbf501bfce74546110c5590943b92bcdc081
RexTrue <114847416+RexTrue@users.noreply.github.com>
2026-06-09 22:00:41 +0700

    Update db.js

--- notes_app/backend/db.js
+++ notes_app/backend/db.js
@@ -1,19 +1,35 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
+
   if (err) {
-    console.error("Database connection failed:", err);
-  } else {
-    console.log("Connected to MySQL Database");
+
+    console.error(
+      "Database connection failed:",
+      err
+    );
+
+    return;
   }
+
+  console.log(
+    "Connected to MySQL Database"
+  );
+
+  connection.release();
 });
 
 module.exports = db;
